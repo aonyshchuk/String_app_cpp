29e37859d92d65bf6bf40fe48a8f39394949aeae
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-12-03 18:13:11 +0300

    in com 1

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,22 @@ using namespace std;
 
 int main()
 {
-    
+   //Завдання 4. Дан рядок символів. Необхідно перевірити, чи є цей рядок паліндромом.
+
+	string str;
+	cout << "Enter the word-> ";
+	cin >> str;
+
+	int len = strlen(str);
+
+	for (int i = 0; i < len/2; ++i)
+	{
+		if (str[i] != str[len - i - 1])
+		{
+			return false;
+		}
+	}
+	return true;
 
 	return 0;
 }
