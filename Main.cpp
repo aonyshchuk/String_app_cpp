285fc59db8dcee48e8661ca3d05b063149c880e5
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-12-03 17:45:21 +0300

    init com

--- Main.cpp
+++ Main.cpp
@@ -3,7 +3,29 @@ using namespace std;
 
 int main()
 {
-    
+    char s[59] = "You should calculate the amount of words in this sentence.";
+
+    int i;
+    int k; 
+    bool f; 
+
+    k = 0; 
+    f = true;
+  
+    for (i = 0; i < strlen(s); i++)
+        if ((s[i] == ' ') || (s[i] == ',') || (s[i] == '.'))
+        {
+            f = true; 
+        }
+        else
+        {
+            if (f)
+            {
+                k++;
+                f = false;
+            }
+        }
+
 
 	return 0;
 }
