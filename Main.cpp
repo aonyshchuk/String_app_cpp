e89f0abb49a16586ea811fa3e69bd3ac17b86b5a
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-12-03 17:37:42 +0300

    Done task 2

--- Main.cpp
+++ Main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 using namespace std; 
-
+//Завдання 2. Дан рядок символів. Визначити кількість літер, цифр та інших символів, присутніх у рядку. 
 int main()
 {
+    setlocale(0, " "); 
     
+    char str1[] = "Визначити кількість літер, цифр та інших символів, присутніх у рядку."; 
+   
 
-	return 0;
+    int len_str1 = strlen(str1); 
+   
+    cout << "str1 = " << len_str1 << endl; 
+  
+    return 0;
 }
