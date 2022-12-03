74f84d3963aa3cc5567db68a4f9c905e115f48f7
aonyshchuk <alinaonyshchuk7@gmail.com>
2022-12-03 18:27:10 +0300

    Done task 4.

--- Main.cpp
+++ Main.cpp
@@ -5,20 +5,21 @@ int main()
 {
    //Завдання 4. Дан рядок символів. Необхідно перевірити, чи є цей рядок паліндромом.
 
-	char s[];
+	char s[50];
 	cout << "Enter the word-> ";
 	cin >> s;
-
+	int i; 
 	int len = strlen(s);
 
 	for (int i = 0; i < len/2; ++i)
 	{
 		if (s[i] != s[len - i - 1])
 		{
-			return false;
+			cout << "Word isn`t polindrom" << endl;
 		}
 	}
-	return true;
+	cout << "Word is polindrom" << endl;
+
 
 	return 0;
-}
+} 
